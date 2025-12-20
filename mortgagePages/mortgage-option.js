4516ba610c9c1d13c67ace9ee9e42b044ef9f8fd
raajpatre <raajpatre27@gmail.com>
2025-12-20 14:56:10 +0530

    Draft 1

---
+++ mortgagePages/mortgage-option.js
@@ -0,0 +1,30 @@
+function select1(){
+    document.getElementById("ans1").setAttribute("style","border:3px solid #2d7b82;color:#2d7b82;font-weight:bold")
+}
+function select2(){
+    document.getElementById("ans2").setAttribute("style","border:3px solid #2d7b82;color:#2d7b82;font-weight:bold")
+}
+function select3(){
+    document.getElementById("ans3").setAttribute("style","border:3px solid #2d7b82;color:#2d7b82;font-weight:bold")
+}
+function select4(){
+    document.getElementById("ans4").setAttribute("style","border:3px solid #2d7b82;color:#2d7b82;font-weight:bold")
+}
+function select5(){
+    document.getElementById("ans5").setAttribute("style","border:3px solid #2d7b82;color:#2d7b82;font-weight:bold")
+}
+function rem1(){
+    document.getElementById("ans1").removeAttribute("style","border:3px solid #2d7b82;color:#2d7b82;font-weight:bold")
+}
+function rem2(){
+    document.getElementById("ans2").removeAttribute("style","border:3px solid #2d7b82;color:#2d7b82;font-weight:bold")
+}
+function rem3(){
+    document.getElementById("ans3").removeAttribute("style","border:3px solid #2d7b82;color:#2d7b82;font-weight:bold")
+}
+function rem4(){
+    document.getElementById("ans4").removeAttribute("style","border:3px solid #2d7b82;color:#2d7b82;font-weight:bold")
+}
+function rem5(){
+    document.getElementById("ans5").removeAttribute("style","border:3px solid #2d7b82;color:#2d7b82;font-weight:bold")
+}
